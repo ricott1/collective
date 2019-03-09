dff4f0c7e6fa1839e869a1ce20532c44ef4640fb
ricott1 <ricott2@gmail.com>
2019-03-09 06:35:41 +0100

    added field and timestamp, distribute funds called by contract owner, no ties resolved

--- contracts/FundingToken.sol
+++ contracts/FundingToken.sol
@@ -7,6 +7,9 @@ import "./ContinuousToken.sol";
 contract FundingToken is ContinuousToken {
     using SafeMath for uint256;
 
+    uint256 public timeframe;
+    uint256 public totalFunds;
+
     event Voting(
         address _from,
         address  _to,
@@ -21,13 +24,19 @@ contract FundingToken is ContinuousToken {
         uint256 votes; 
         uint256 minPayment;
         uint256 maxPayment; 
+        uint field;
+        uint256 time;
         uint pointer;      
     }
 
     mapping (address => Project) public projects;
     address[] public projectList;
+
     
-    constructor(uint256 _reserveRatio) ContinuousToken(_reserveRatio) public {}
+    constructor(uint256 _reserveRatio, uint256 _timeframe) ContinuousToken(_reserveRatio) public {
+        timeframe = _timeframe;
+        totalFunds = 0;
+    }
 
     function isProject(address addr) 
         public 
@@ -43,19 +52,50 @@ contract FundingToken is ContinuousToken {
     function getProjectByAddress(address addr)
         public
         view
-        returns(uint256 votes, uint256 min, uint256 max)
+        returns(uint256 votes, uint256 min, uint256 max, uint field, uint256 time)
     {
-        return (projects[addr].votes, projects[addr].minPayment, projects[addr].maxPayment);
+        Project memory p = projects[addr];
+        return (p.votes, p.minPayment, p.maxPayment, p.field, p.time);
     }
 
     function getProjectByIndex(uint index)
         public
         view
-        returns(uint256 votes, uint256 min, uint256 max)
+        returns(uint256 votes, uint256 min, uint256 max, uint field, uint256 time)
     {
         require (index >= 0 && index < getProjectCount(), "Invalid index");
         address _addr = projectList[index];
-        return (projects[_addr].votes, projects[_addr].minPayment, projects[_addr].maxPayment);
+        Project memory p = projects[_addr];
+        return (p.votes, p.minPayment, p.maxPayment, p.field, p.time);
+    }
+
+    function getProjectVotesByAddress(address addr)
+        public
+        view
+        returns(uint256 votes)
+    {
+        Project memory p = projects[addr];
+        return p.votes;
+    }
+
+    function getProjectVotesByIndex(uint index)
+        public
+        view
+        returns(uint256 votes)
+    {
+        require (index >= 0 && index < getProjectCount(), "Invalid index");
+        address _addr = projectList[index];
+        Project memory p = projects[_addr];
+        return p.votes;
+    }
+
+    function getProjectPointerByAddress(address addr)
+        public
+        view
+        returns(uint pointer)
+    {
+        Project memory p = projects[addr];
+        return p.pointer;
     }
 
 
@@ -65,18 +105,20 @@ contract FundingToken is ContinuousToken {
         require(amount > 0, "Must spend tokens to vote.");
         //transfer amount to smart contract
         transfer(owner(), amount);
+        totalFunds += amount;
         projects[addr].votes += amount;
         emit Voting(msg.sender, addr, amount);
         return true;
     }
 
-    function newProject (uint256 min, uint256 max) 
+    function newProject (uint256 min, uint256 max, uint field) 
+    //can create or fund a project only when not distributing
         external returns(bool res)  
     {
         require(msg.sender != owner(), "Contract owner cannot create a new project");
         //check the user didn't submit another project already. We are gonna extend this to allow for multiple projects per user/account (maybe)
         require(!isProject(msg.sender), "Applicant already sent a project, delete it before submitting a new one");
-        Project memory _newProject = Project({votes:0, minPayment:min, maxPayment:max, pointer:0});
+        Project memory _newProject = Project({votes:0, minPayment:min, maxPayment:max, field:field, time:now, pointer:0});
         projects[msg.sender] = _newProject;
         projects[msg.sender].pointer = projectList.push(msg.sender) - 1;
         emit NewProject(msg.sender);
@@ -87,7 +129,7 @@ contract FundingToken is ContinuousToken {
         return projectList.length;
     }
 
-    function deleteProjectByAddress(address addr) public returns(bool success) {
+    function deleteProjectByAddress(address addr) public onlyOwner returns(bool success) {
         require(isProject(addr), "Project not present, cannot delete it");
         uint rowToDelete = projects[addr].pointer;
         address keyToMove   = projectList[projectList.length-1];
@@ -96,6 +138,34 @@ contract FundingToken is ContinuousToken {
         projectList.length--;
         return true;
     }
+
+    function distributeFunds () public onlyOwner returns(bool res) {
+        uint256 prize = totalFunds;
+        address winner = getMostVoted();
+        //make sure ties get the same prize
+        projects[winner].votes = 0;
+        transfer(winner, prize);
+        totalFunds = 0;
+        return true;
+    }
+
+    function getMostVoted() internal view returns(address) {
+        uint256 largest = 0; 
+        uint256 i;
+        address winner;
+
+        for(i = 0; i < projectList.length; i++){
+            uint256 total = getProjectVotesByIndex(i);
+            if(total > largest) {
+                largest = total; 
+                winner = projectList[i];
+            } 
+        }
+
+        return winner;
+    }
+    
+
 
           
 }
