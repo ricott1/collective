7fe716216bb113058f24b426ba71e6ac6126d17a
Terrazzoni Jean-Baptiste <jterrazz@protonmail.com>
2019-03-09 14:54:58 +0100

    New contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
-var DB = artifacts.require("./DB.sol");
+const Power = artifacts.require("Power");
+const BancorBondingCurve = artifacts.require("BancorBondingCurve");
+const CappedGasPrice = artifacts.require("CappedGasPrice");
+const ContinuousToken = artifacts.require("ContinuousToken");
 
-module.exports = function (deployer) {
-  deployer.deploy(DB);
+module.exports = function(deployer) {
+  deployer.deploy(Power);
+  deployer.deploy(BancorBondingCurve);
+  deployer.deploy(CappedGasPrice);
+  deployer.deploy(ContinuousToken, 10000);
 };
