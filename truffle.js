9f0a7a9517c76a748766f9f63203ff11b86c0081
ricott1 <ricott2@gmail.com>
2019-03-09 15:15:32 +0100

    big merge back and frontend

--- truffle.js
+++ truffle.js
@@ -35,9 +35,14 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545, // This is the conventional port. If you're using the Ganache Blockchain, change port value to the Ganache default port 7545. If you're using Truffle develop network, change port value to 9545
-      network_id: "5777", // Match any network id. You may need to replace * with your network Id
+      network_id: "*", // Match any network id. You may need to replace * with your network Id
       from: "", // Add your unlocked account within the double quotes
       gas: 444444400
     }
-  }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.2",
+    },
+  },
 };
