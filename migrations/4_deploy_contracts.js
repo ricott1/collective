0afb5b9f357ed1c8eacf04637fa554fe6e05030c
Terrazzoni Jean-Baptiste <jterrazz@protonmail.com>
2019-03-09 12:48:37 +0100

    Vue build setup

---
+++ migrations/4_deploy_contracts.js
@@ -0,0 +1,11 @@
+const Power = artifacts.require("Power");
+const BancorBondingCurve = artifacts.require("BancorBondingCurve");
+const CappedGasPrice = artifacts.require("CappedGasPrice");
+const ContinuousToken = artifacts.require("ContinuousToken");
+
+module.exports = function(deployer) {
+  deployer.deploy(Power);
+  deployer.deploy(BancorBondingCurve);
+  deployer.deploy(CappedGasPrice);
+  deployer.deploy(ContinuousToken, 500000);
+};
--- truffle.js
+++ truffle.js
@@ -37,7 +37,7 @@ module.exports = {
       port: 8545, // This is the conventional port. If you're using the Ganache Blockchain, change port value to the Ganache default port 7545. If you're using Truffle develop network, change port value to 9545
       network_id: "5777", // Match any network id. You may need to replace * with your network Id
       from: "", // Add your unlocked account within the double quotes
-      gas: 4444444
+      gas: 44444444
     }
   }
 };
