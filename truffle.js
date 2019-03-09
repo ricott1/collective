7a3bd97d0ae16650df6b163cd4423e4f455eb0bd
mi-kas <mi.kaserer@gmail.com>
2019-03-10 00:55:06 +0100

    Modifications

--- truffle.js
+++ truffle.js
@@ -1,28 +1,28 @@
-var bip39 = require("bip39");
-var hdkey = require('ethereumjs-wallet/hdkey');
-var ProviderEngine = require("web3-provider-engine");
-var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
-var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
-var Web3 = require("web3");
-const FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
+var bip39 = require('bip39')
+var hdkey = require('ethereumjs-wallet/hdkey')
+var ProviderEngine = require('web3-provider-engine')
+var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js')
+var Web3Subprovider = require('web3-provider-engine/subproviders/web3.js')
+var Web3 = require('web3')
+const FilterSubprovider = require('web3-provider-engine/subproviders/filters.js')
 
 // Get our mnemonic and create an hdwallet
-var mnemonic = "piano file obey immense polar rack great subject clutch camera maid ostrich";
-var hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
+var mnemonic = 'piano file obey immense polar rack great subject clutch camera maid ostrich'
+var hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic))
 
 // Get the first account using the standard hd path.
-var wallet_hdpath = "m/44'/60'/0'/0/";
-var wallet = hdwallet.derivePath(wallet_hdpath + "0").getWallet();
-var address = "0x" + wallet.getAddress().toString("hex");
+var wallet_hdpath = "m/44'/60'/0'/0/"
+var wallet = hdwallet.derivePath(wallet_hdpath + '0').getWallet()
+var address = '0x' + wallet.getAddress().toString('hex')
 
-var providerUrl = "https://testnet.infura.io";
-var engine = new ProviderEngine();
+var providerUrl = 'https://testnet.infura.io'
+var engine = new ProviderEngine()
 // filters
-engine.addProvider(new FilterSubprovider());
+engine.addProvider(new FilterSubprovider())
 
-engine.addProvider(new WalletSubprovider(wallet, {}));
-engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)));
-engine.start(); // Required by the provider engine.
+engine.addProvider(new WalletSubprovider(wallet, {}))
+engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)))
+engine.start() // Required by the provider engine.
 
 module.exports = {
   networks: {
@@ -33,24 +33,22 @@ module.exports = {
     //   gas: 4444444
     // },
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545, // This is the conventional port. If you're using the Ganache Blockchain, change port value to the Ganache default port 7545. If you're using Truffle develop network, change port value to 9545
-      network_id: "*", // Match any network id. You may need to replace * with your network Id
-      from: "", // Add your unlocked account within the double quotes
-      gas: 444444400
+      network_id: '*', // Match any network id. You may need to replace * with your network Id
+      from: '', // Add your unlocked account within the double quotes
+      gas: 444444400,
     },
     skale: {
-      provider: new Web3.providers.HttpProvider(
-        "http://104.248.242.122:8003/"
-      ),
+      provider: new Web3.providers.HttpProvider('http://104.248.242.122:8003/'),
       gasPrice: 0,
       gas: 8000000,
-      network_id: "*"
-    }
+      network_id: '*',
+    },
   },
   compilers: {
     solc: {
-      version: "0.5.2",
+      version: '0.5.2',
     },
   },
-};
+}
