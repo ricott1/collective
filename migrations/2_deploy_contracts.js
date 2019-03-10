a20f335badcdd1923e3a919cf72e7e739cbf7bb3
ricott1 <ricott2@gmail.com>
2019-03-10 02:09:48 +0100

    fixed last sub

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
-const Power = artifacts.require('Power')
-const BancorBondingCurve = artifacts.require('BancorBondingCurve')
-const CappedGasPrice = artifacts.require('CappedGasPrice')
-const ContinuousToken = artifacts.require('ContinuousToken')
+const Power = artifacts.require('Power');
+const BancorBondingCurve = artifacts.require('BancorBondingCurve');
+const CappedGasPrice = artifacts.require('CappedGasPrice');
+const ContinuousToken = artifacts.require('ContinuousToken');
+const FundingToken = artifacts.require("FundingToken");
 
 module.exports = function(deployer) {
 	deployer.deploy(Power)
 	deployer.deploy(BancorBondingCurve)
 	deployer.deploy(CappedGasPrice)
 	deployer.deploy(ContinuousToken, 10000)
+	deployer.deploy(FundingToken, 10000, 60)
 }
